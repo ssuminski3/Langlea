57c7e177ecb1232d44ee234187c26877ca651198
ssuminski3 <ssuminski3@gmail.com>
2024-09-28 20:35:00 +0200

    Changed navigation to learning type.

--- app/type.js
+++ app/type.js
@@ -5,22 +5,18 @@ import { router } from 'expo-router';
 
 export default function App() {
     const local = useLocalSearchParams()
-    const lang  = local.language
+    const lang = local.language
     const sub = local.subject
-    const types = ["Dialog", "Czytanie ze zrozumieniem"]
     return (
         <View className="bg-slate-700 justify-center flex-1 items-center">
             <Text className="text-white text-3xl m-3 text-center">What type of learning do you prefer in {lang} of {sub}?</Text>
             <ScrollView>
-                {
-                    types.map(t => {
-                        return (
-                            <TouchableOpacity key={t} className="bg-slate-900 p-10  mt-3" onPress={() => router.navigate({ pathname: './learning', params: { language: lang, subject: sub, type: t }})}>
-                                <Text className="text-white w-full text-center text-2xl">{t}</Text>
-                            </TouchableOpacity>
-                        )
-                    })
-                }
+                <TouchableOpacity className="bg-slate-900 p-10  mt-3" onPress={() => router.navigate({ pathname: './dialog', params: { language: lang, subject: sub, type: "Dialog" } })}>
+                    <Text className="text-white w-full text-center text-2xl">Dialog</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-slate-900 p-10  mt-3" onPress={() => router.navigate({ pathname: './reading', params: { language: lang, subject: sub, type: "Czytanie ze zrozumieniem" } })}>
+                    <Text className="text-white w-full text-center text-2xl">Czytanie ze zrozumieniem</Text>
+                </TouchableOpacity>
             </ScrollView>
         </View>
     );
